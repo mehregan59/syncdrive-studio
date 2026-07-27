b62e7f08991c07e16c71f595573500f09743d2b5
Mehregan <90446634+mehregan59@users.noreply.github.com>
2026-07-27 17:57:18 +0200

    Clean up comments and formatting in drive_detector.py
    Removed commented docstrings and updated formatting.

--- drive_detector.py
+++ drive_detector.py
@@ -5,7 +5,6 @@ from PyQt6.QtCore import QThread, pyqtSignal
 
 
 class DriveWatcherThread(QThread):
-    # Emits drive mountpoint (e.g. "E:\\" on Windows)
     drive_connected = pyqtSignal(str)
 
     def __init__(self, check_interval: int = 3):
@@ -15,14 +14,12 @@ class DriveWatcherThread(QThread):
         self.known_drives: Set[str] = self._get_current_drives()
 
     def _get_current_drives(self) -> Set[str]:
-        """Returns set of connected drive mountpoints."""
         try:
             return {p.mountpoint for p in psutil.disk_partitions(all=False)}
         except Exception:
             return set()
 
     def run(self):
-        """Monitors system for newly plugged-in drives."""
         while self._running:
             time.sleep(self.check_interval)
             current_drives = self._get_current_drives()
